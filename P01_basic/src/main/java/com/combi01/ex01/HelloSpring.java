5aea4c94c5ce3645baa915ca1de7e6c173285cbe
dltmd <dltmd@LAPTOP-NOJP8KAN>
2022-05-16 21:14:40 +0900

    first try git

---
+++ P01_basic/src/main/java/com/combi01/ex01/HelloSpring.java
@@ -0,0 +1,28 @@
+package com.combi01.ex01;
+
+public class HelloSpring {
+
+	public static void main(String[] args) {
+		
+		MessageBeanEn beanA = new MessageBeanEn();
+		beanA.sayHello();
+		
+		System.out.println();
+		
+		MessageBeanKr beanB = new MessageBeanKr();
+		beanB.sayHello();
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ P01_basic/src/main/java/com/combi01/ex01/MessageBeanEn.java
@@ -0,0 +1,13 @@
+
+
+
+
+package com.combi01.ex01;
+
+public class MessageBeanEn {
+
+	public void sayHello() {
+		System.out.println("hi~ spring...");
+	}
+	
+}
